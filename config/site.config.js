c87dd598190c2cd8e0b5d9aa48fd95f5fd3cf622
onlineDrive <154478283+onlineDrive@users.noreply.github.com>
2024-01-17 12:56:30 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Personal'],
+  protectedRoutes: ['/Personal', 'Audio_backup'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:ishare0809@gmail.com',
